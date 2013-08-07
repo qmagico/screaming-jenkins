e9869c3802ee962456f6527e98bbb98cce8b39b4
Denis Costa <deniscostadsc@gmail.com>
2013-08-07 12:13:15 -0300

    Printing branch names

--- screaming.py
+++ screaming.py
@@ -20,7 +20,11 @@ for tool in space_tools:
 
 merge_requests = api.merge_requests(space_id=aulasdovaca.id, space_tool_id=tool.id)
 
+branch_names = []
+
 for merge in merge_requests:
     delta = datetime.now() - merge.updated_at
     if delta.total_seconds() / 60 / 60 >= 6:
-        exit(1)
+        branch_names.append(merge.source_symbol)
+
+print(', '.join(branch_names))
